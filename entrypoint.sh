4584d790dd090804f5ad4d1312585e56f4f3334f
destrex271 <destrex271@gmail.com>
2023-08-31 22:33:28 +0530

    Echo summary report

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,9 @@ pip install -r requirements.txt
 touch analysis.txt
 result=$(python3 main.py $1 $2)
 
-result=$(echo $result | tr '\n' ' ')
-echo "analysis=$result" >> $GITHUB_OUTPUT
+result2=$(echo $result | tr '\n' ' ')
+echo "analysis=$result2" >> $GITHUB_OUTPUT
 
-echo "$?"
+echo "$result" >> result.txt
 exit $?
 
