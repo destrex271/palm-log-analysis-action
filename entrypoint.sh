dac8356ef96449705c7bb5c7ac4c322b9c174b1f
destrex271 <destrex271@gmail.com>
2023-09-02 00:37:13 +0530

    Fix script path

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,10 @@ api_key=$2
 
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y python3 python3-pip
-pip install -r requirements.txt
+pip install -r $GITHUB_WORKSPACE/requirements.txt
 
 touch analysis.txt
-result=$(python3 $GITHUB_PATH/main.py $1 $2)
+result=$(python3 $GITHUB_WORKSPACE/main.py $1 $2)
 
 touch result.txt
 echo "$result" >> result.txt
